d7cb882a312aa40a7640779e12946afa514f921f
michaelgathara <mikegtrm@gmail.com>
2024-09-21 04:24:51 -0500

    fixing model errors

--- ai_podcast_generator/config.py
+++ ai_podcast_generator/config.py
@@ -26,4 +26,4 @@ HOST_1_NAME = "Alice"
 HOST_2_NAME = "Bob"
 OPEN_AI_MODEL= "gpt-4o-mini"
 CLAUDE_MODEL = "claude-2.1"
-USER_CHOICE_MODEL = 1 # 0 is OpenAI and 1 is Claude
+USER_CHOICE_MODEL = 0 # 0 is OpenAI and 1 is Claude
